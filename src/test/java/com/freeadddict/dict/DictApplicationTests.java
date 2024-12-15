4730e2cdd6240770c38395b703f416332d77e70e
leejj2jj <leejj2jj@gmail.com>
2024-12-16 04:36:20 +0900

    chore: 프로젝트 생성

---
+++ src/test/java/com/freeadddict/dict/DictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeadddict.dict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
