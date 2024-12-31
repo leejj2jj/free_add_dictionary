7a6bd111a9d40e86ef301b31ac04c870d052cc0d
leejj2jj <leejj2jj@gmail.com>
2024-12-31 15:11:52 +0900

    style: 들여쓰기를 tabs:2에서 spaces:2로 변경

--- src/main/java/com/freeadddictionary/dict/DictApplication.java
+++ src/main/java/com/freeadddictionary/dict/DictApplication.java
@@ -8,8 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class DictApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DictApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DictApplication.class, args);
+  }
 
 }
