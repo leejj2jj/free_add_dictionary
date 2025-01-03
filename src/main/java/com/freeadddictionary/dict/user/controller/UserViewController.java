5dd8cb1585ca64de688a90f89fe2a207a114effe
leejj2jj <leejj2jj@gmail.com>
2025-01-03 17:15:02 +0900

    refactor: User 기본 생성자 protected로 변경

--- src/main/java/com/freeadddictionary/dict/user/controller/UserViewController.java
+++ src/main/java/com/freeadddictionary/dict/user/controller/UserViewController.java
@@ -1,27 +1,22 @@
 package com.freeadddictionary.dict.user.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-import com.freeadddictionary.dict.user.domain.User;
+import com.freeadddictionary.dict.user.dto.AddUserRequest;
+import com.freeadddictionary.dict.user.dto.LoginUserRequest;
 
 @Controller
 public class UserViewController {
 
-  @ModelAttribute("user")
-  public User user() {
-    return new User();
-  }
-
   @GetMapping("/login")
-  public String login(Model model) {
+  public String login(@ModelAttribute("user") LoginUserRequest user) {
     return "user/login";
   }
 
   @GetMapping("/signup")
-  public String signup(Model model) {
+  public String signup(@ModelAttribute("user") AddUserRequest user) {
     return "user/signup";
   }
 
--- src/main/java/com/freeadddictionary/dict/user/domain/User.java
+++ src/main/java/com/freeadddictionary/dict/user/domain/User.java
@@ -27,12 +27,13 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Table(name = "users")
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @Entity
