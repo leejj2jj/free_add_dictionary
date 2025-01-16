aed1153f95969b739bd25eac29ed098c287646fb
leejj2jj <leejj2jj@gmail.com>
2025-01-16 12:39:08 +0900

    refactor: @OneToOne에 FetchType.LAZY 추가

--- src/main/java/com/freeadddictionary/dict/report/domain/Report.java
+++ src/main/java/com/freeadddictionary/dict/report/domain/Report.java
@@ -1,5 +1,6 @@
 package com.freeadddictionary.dict.report.domain;
 
+import static jakarta.persistence.FetchType.LAZY;
 import static lombok.AccessLevel.PROTECTED;
 
 import java.time.LocalDateTime;
@@ -17,7 +18,6 @@ import com.freeadddictionary.dict.word.domain.ReportedWord;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -54,11 +54,11 @@ public class Report {
   @LastModifiedDate
   private LocalDateTime modifyDate;
 
-  @ManyToOne(fetch = FetchType.LAZY)
+  @ManyToOne(fetch = LAZY)
   @JoinColumn(name = "user_id")
   private User user;
 
-  @OneToOne(mappedBy = "report")
+  @OneToOne(mappedBy = "report", fetch = LAZY)
   private ReportReply reportReply;
 
   @OneToMany(mappedBy = "report")
