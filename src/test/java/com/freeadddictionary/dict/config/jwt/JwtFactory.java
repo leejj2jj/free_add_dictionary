4077e9db7a1b6fff90599f28f850c8f270a8bb6d
leejj2jj <leejj2jj@gmail.com>
2025-01-16 16:26:46 +0900

    style: 들여쓰기 space 2로 변경

--- src/test/java/com/freeadddictionary/dict/config/jwt/JwtFactory.java
+++ src/test/java/com/freeadddictionary/dict/config/jwt/JwtFactory.java
@@ -13,26 +13,26 @@ import lombok.Getter;
 
 @Getter
 public class JwtFactory {
-    private String subject = "test@email.com";
-    private Date issuedAt = new Date();
-    private Date expiration = new Date(new Date().getTime() + Duration.ofDays(14).toMillis());
-    private Map<String, Object> claims = Collections.emptyMap();
+  private String subject = "test@email.com";
+  private Date issuedAt = new Date();
+  private Date expiration = new Date(new Date().getTime() + Duration.ofDays(14).toMillis());
+  private Map<String, Object> claims = Collections.emptyMap();
 
-    @Builder
-    public JwtFactory(String subject, Date issuedAt, Date expiration, Map<String, Object> claims) {
-        this.subject = subject != null ? subject : this.subject;
-        this.issuedAt = issuedAt != null ? issuedAt : this.issuedAt;
-        this.expiration = expiration != null ? expiration : this.expiration;
-        this.claims = claims != null ? claims : this.claims;
-    }
+  @Builder
+  public JwtFactory(String subject, Date issuedAt, Date expiration, Map<String, Object> claims) {
+    this.subject = subject != null ? subject : this.subject;
+    this.issuedAt = issuedAt != null ? issuedAt : this.issuedAt;
+    this.expiration = expiration != null ? expiration : this.expiration;
+    this.claims = claims != null ? claims : this.claims;
+  }
 
-    public static JwtFactory withDefaultValues() {
-        return JwtFactory.builder().build();
-    }
+  public static JwtFactory withDefaultValues() {
+    return JwtFactory.builder().build();
+  }
 
-    public String createToken(JwtProperties jwtProperties) {
-        return Jwts.builder().setSubject(subject).setHeaderParam(Header.TYPE, Header.JWT_TYPE)
-                .setIssuer(jwtProperties.getIssuer()).setIssuedAt(issuedAt).setExpiration(expiration).addClaims(claims)
-                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey()).compact();
-    }
+  public String createToken(JwtProperties jwtProperties) {
+    return Jwts.builder().setSubject(subject).setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+        .setIssuer(jwtProperties.getIssuer()).setIssuedAt(issuedAt).setExpiration(expiration).addClaims(claims)
+        .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey()).compact();
+  }
 }
