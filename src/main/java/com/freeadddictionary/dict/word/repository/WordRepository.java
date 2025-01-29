f0e5be44f9b488f4a54e231b68216ef9ca68ad0e
leejj2jj <leejj2jj@gmail.com>
2025-01-30 01:07:11 +0900

    refactor: 쓰이지 않는 메서드 삭제

--- src/main/java/com/freeadddictionary/dict/word/repository/WordRepository.java
+++ src/main/java/com/freeadddictionary/dict/word/repository/WordRepository.java
@@ -1,12 +1,8 @@
 package com.freeadddictionary.dict.word.repository;
 
 import com.freeadddictionary.dict.word.domain.Word;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WordRepository extends JpaRepository<Word, Long> {
-
-  Optional<Word> findByName(String name);
-}
+public interface WordRepository extends JpaRepository<Word, Long> {}
