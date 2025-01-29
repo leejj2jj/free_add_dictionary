b797cca2ee28a019e6651622577c6678a7f0a1b7
leejj2jj <leejj2jj@gmail.com>
2025-01-30 01:07:38 +0900

    feat: 비밀번호 조건 정규식 추가

--- src/main/java/com/freeadddictionary/dict/user/dto/request/RegisterUserRequest.java
+++ src/main/java/com/freeadddictionary/dict/user/dto/request/RegisterUserRequest.java
@@ -3,6 +3,7 @@ package com.freeadddictionary.dict.user.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,6 +20,7 @@ public class RegisterUserRequest {
 
   @NotBlank
   @Size(min = 10, max = 60)
+  @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{10,}$")
   private String password;
 
   @NotBlank
