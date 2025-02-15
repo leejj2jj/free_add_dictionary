c494a547f71903cf8ab3abeab26a3cb844926ca0
leejj2jj <leejj2jj@gmail.com>
2025-02-15 18:09:51 +0900

    refactor: JpaRepository 상속 리포지터리들의 Repository 애너테이션 삭제

--- src/main/java/com/freeadddictionary/dict/report/repository/ReportRepository.java
+++ src/main/java/com/freeadddictionary/dict/report/repository/ReportRepository.java
@@ -2,7 +2,5 @@ package com.freeadddictionary.dict.report.repository;
 
 import com.freeadddictionary.dict.report.domain.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {}
--- src/main/java/com/freeadddictionary/dict/word/repository/WordRepository.java
+++ src/main/java/com/freeadddictionary/dict/word/repository/WordRepository.java
@@ -2,7 +2,5 @@ package com.freeadddictionary.dict.word.repository;
 
 import com.freeadddictionary.dict.word.domain.Word;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface WordRepository extends JpaRepository<Word, Long> {}
