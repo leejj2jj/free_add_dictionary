72fcd6a6f546e7b2d4d2b436f6b83aa4df182788
leejj2jj <leejj2jj@gmail.com>
2025-02-24 03:56:04 +0900

    fix: 캐시 설정 효율화

--- src/main/java/com/freeadddictionary/dict/config/CacheConfig.java
+++ src/main/java/com/freeadddictionary/dict/config/CacheConfig.java
@@ -1,10 +1,10 @@
 package com.freeadddictionary.dict.config;
 
-import java.util.Arrays;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.concurrent.TimeUnit;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,12 +14,21 @@ public class CacheConfig {
 
   @Bean
   public CacheManager cacheManager() {
-    SimpleCacheManager cacheManager = new SimpleCacheManager();
-    cacheManager.setCaches(
-        Arrays.asList(
-            new ConcurrentMapCache("statistics"),
-            new ConcurrentMapCache("dictionary"),
-            new ConcurrentMapCache("reports")));
+    CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+    cacheManager.setCaffeine(
+        Caffeine.newBuilder().expireAfterWrite(60, TimeUnit.MINUTES).maximumSize(100));
+
+    cacheManager.setCacheNames(
+        java.util.Arrays.asList("statistics", "dictionary", "reports", "admins"));
+
     return cacheManager;
+  }
+
+  @Bean
+  public Caffeine caffeineConfig() {
+    return Caffeine.newBuilder()
+        .expireAfterWrite(60, TimeUnit.MINUTES)
+        .initialCapacity(100)
+        .maximumSize(500);
   }
 }
