6ef6299d6759a004d74ffce9e28905cafc8fc505
leejj2jj <leejj2jj@gmail.com>
2025-02-28 02:02:12 +0900

    refactor: 중복된 기능을 하는 TestConfig 통합

--- src/test/java/com/freeadddictionary/dict/config/TestConfig.java
+++ src/test/java/com/freeadddictionary/dict/config/TestConfig.java
@@ -2,12 +2,15 @@ package com.freeadddictionary.dict.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
 @TestConfiguration
 public class TestConfig {
+
   @Bean
   public SpringTemplateEngine templateEngine() {
     SpringTemplateEngine templateEngine = new SpringTemplateEngine();
@@ -23,5 +26,13 @@ public class TestConfig {
     templateResolver.setTemplateMode(TemplateMode.HTML);
     templateResolver.setCacheable(false);
     return templateResolver;
+  }
+
+  @Bean
+  public ViewResolver viewResolver() {
+    ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+    resolver.setTemplateEngine(templateEngine());
+    resolver.setCharacterEncoding("UTF-8");
+    return resolver;
   }
 }
